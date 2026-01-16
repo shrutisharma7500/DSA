3c4befc0c7e5546f0050b9ba678fce429f59f64f
Shruti sharma <shruti.sharma.cseaiml.2022@miet.ac.in>
2026-01-16 18:41:55 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (94.42%) - LeetSync

---
+++ 792-binary-search/binary-search.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left=0;
+        int right=nums.Length-1;
+        while(left <= right){
+            int mid=left+(right-left)/2;
+            if(target == nums[mid]){
+                return mid;
+            }else if(target< nums[mid]){
+                 right=mid-1;
+            }else{
+                left=mid+1;
+            };
+        }
+      return -1;  
+    }
+}
